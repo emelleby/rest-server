3c9abe967eaca1621a515d806e3e8ac59c2aee49
Eivind <emelleby@gmail.com>
2016-04-05 21:12:57 +0700

    Final after facebook OAth2

--- config.js
+++ config.js
@@ -2,3 +2,13 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion'
 }
+
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1572886043004740',
+        clientSecret: 'bbf3ed02f735d697a726f1b123c769fe',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
